358ea985bc29abbc363d3d6650f6e4a44fb393b8
Lanlan <liangyuqin1212@gmail.com>
2017-04-19 09:12:50 +0800

    添加裁切比例

--- src/components/Vsidebar.vue
+++ src/components/Vsidebar.vue
@@ -4,23 +4,21 @@
     <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" theme="dark">
       <el-submenu index="1">
         <template slot="title"><i class="my-icon-cut my-icon"></i>裁切</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        <el-submenu index="1-1">
+          <template slot="title">固定比例</template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1-1">16:9</el-menu-item>
+            <el-menu-item index="1-1-2">4:3</el-menu-item>
+            <el-menu-item index="1-1-3">1:1</el-menu-item>
+            <el-menu-item index="1-1-4">2:3</el-menu-item>
+          </el-menu-item-group>
         </el-submenu>
+        <el-menu-item index="1-2">自由裁切</el-menu-item>
       </el-submenu>
       <el-submenu index="2">
         <template slot="title"><i class="my-icon-edit my-icon"></i>调整</template>
         <el-submenu index="2-1">
-          <template slot="title" class="v-submenu">亮度 / 对比度</template>
+          <template slot="title">亮度 / 对比度</template>
           <el-menu-item-group>
             <el-menu-item index="2-1-1">
               <slider></slider>
@@ -31,7 +29,7 @@
           </el-menu-item-group>
         </el-submenu>
         <el-submenu index="2-2">
-          <template slot="title" class="v-submenu">色相 / 饱和度</template>
+          <template slot="title">色相 / 饱和度</template>
           <el-menu-item-group>
             <el-menu-item index="2-2-1">色相</el-menu-item>
             <el-menu-item index="2-2-2">饱和度</el-menu-item>
