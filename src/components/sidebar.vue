a981d2383dd814d7e3e9e92deaa57553e9d0e8c1
Lanlan <liangyuqin1212@gmail.com>
2017-05-25 17:22:09 +0800

    update

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -166,37 +166,37 @@
         filterList: [
           {
             name: 'sunrise',
-            preview: '../../static/image/filter-sunrise.jpg',
+            preview: '/image/filter-sunrise.jpg',
             desc: '日出辉映',
           },
           {
             name: 'lomo',
-            preview: '../../static/image/filter-lomo.jpg',
+            preview: '/image/filter-lomo.jpg',
             desc: 'Lomo风格',
           },
           {
             name: 'clarity',
-            preview: '../../static/image/filter-clarity.jpg',
+            preview: '/image/filter-clarity.jpg',
             desc: '这一刻 更清晰',
           },
           {
             name: 'hazyDays',
-            preview: '../../static/image/filter-hazyDays.jpg',
+            preview: '/image/filter-hazyDays.jpg',
             desc: '这一刻 更朦胧',
           },
           {
             name: 'crossProcess',
-            preview: '../../static/image/filter-crossProcess.jpg',
+            preview: '/image/filter-crossProcess.jpg',
             desc: '怀旧电影',
           },
           {
             name: 'concentrate',
-            preview: '../../static/image/filter-concentrate.jpg',
+            preview: '/image/filter-concentrate.jpg',
             desc: '美式咖啡',
           },
           {
             name: 'jarques',
-            preview: '../../static/image/filter-jarques.jpg',
+            preview: '/image/filter-jarques.jpg',
             desc: '抑郁深蓝',
           },
         ],
